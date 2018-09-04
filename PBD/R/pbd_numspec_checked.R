ba72b9059eea1befdb929e23c42a7c02e30737e8
richelbilderbeek <richel@richelbilderbeek.nl>
2018-09-04 09:40:26 +0200

    Allow vectorized use of 'pbd_numspec_mean_checked'

--- PBD/R/pbd_numspec_checked.R
+++ PBD/R/pbd_numspec_checked.R
@@ -1,6 +1,78 @@
 #' Calcates the mean number of species under protracted birth-death model of
 #' diversification. This is a checked version with a more explicit interface
 #' than \code{\link{pbd_numspec_mean}}.
+#' @param sirs one or more speciation initiation rates.
+#'   This function assumes the speciation initiation rates of good and incipient species are equal.
+#' @param ergs one or more good species' extinction rates
+#' @param scrs one or more speciation completion rates
+#' @param eris one or more incipient species' extinction rates
+#' @param crown_ages one or more crown ages
+#' @return The expected number of representative species
+#' @seealso
+#'   \code{\link{pbd_numspec_mean}} provides for setting a time-dependence
+#'   in the parameters and/or specify a stem age.
+#'   \code{\link{pbd_numspec_median_checked}} calculates the median number of species.
+#' @examples
+#'   n <- pbd_numspec_mean_checked(
+#'     ergs = 0.1,
+#'     eris = 0.2,
+#'     scrs = 0.3,
+#'     sirs = 0.4,
+#'     crown_ages = 0.5
+#'   )
+#'   testthat::expect_equal(n, 1.029111871)
+#'
+#'   n <- pbd_numspec_mean_checked(
+#'     ergs = 0.2,
+#'     eris = 0.4,
+#'     scrs = 0.6,
+#'     sirs = 0.8,
+#'     crown_ages = 1.0
+#'   )
+#'   testthat::expect_equal(n, 1.440746567)
+#'
+#'   # Vectorized use
+#'   ns <- pbd_numspec_mean_checked(
+#'     ergs = c(0.1, 0.2),
+#'     eris = c(0.2, 0.4),
+#'     scrs = c(0.3, 0.6),
+#'     sirs = c(0.4, 0.8),
+#      crown_ages = c(0.5, 1.0)
+#'   )
+#'   testthat::expect_equal(ns, c(1.029111871, 1.440746567))
+#' @author Richel J.C. Bilderbeek
+#' @export
+pbd_numspec_mean_checked <- function(
+  ergs,
+  eris,
+  scrs,
+  sirs,
+  crown_ages
+) {
+  if (any(sirs < 0.0)) stop("All 'sirs' must be positive")
+  if (any(ergs < 0.0)) stop("All 'ergs' must be positive")
+  if (any(scrs < 0.0)) stop("All 'scrs' must be positive")
+  if (any(eris < 0.0)) stop("All 'eris' must be positive")
+  if (any(crown_ages < 0.0)) stop("All 'crown_ages' must be positive")
+  if (length(ergs) != length(eris)) stop("ergs' and 'eris' must have same length")
+  if (length(ergs) != length(scrs)) stop("ergs' and 'scrs' must have same length")
+  if (length(ergs) != length(sirs)) stop("ergs' and 'sirs' must have same length")
+  if (length(ergs) != length(crown_ages)) stop("ergs' and 'crown_ages' must have same length")
+  n <- length(ergs)
+  ns <- rep(NA, n)
+  for (i in seq_along(ergs)) {
+    ns[i] <- pbd_numspec_mean_checked_impl(
+      erg = ergs[i],
+      eri = eris[i],
+      scr = scrs[i],
+      sir = sirs[i],
+      crown_age = crown_ages[i]
+    )
+  }
+  ns
+}
+
+#' Implementation of \code{\link{pbd_numspec_mean_checked}}.
 #' @param sir speciation initiation rate. This function assumes the
 #'   speciation initiation rates of good and incipient species are equal.
 #' @param erg extinction rate of a good species
@@ -13,7 +85,7 @@
 #'   in the parameters and/or specify a stem age.
 #'   \code{\link{pbd_numspec_median_checked}} calculates the median number of species.
 #' @examples
-#'  mean_n_species <- pbd_numspec_mean_checked(
+#'  mean_n_species <- pbd_numspec_mean_checked_impl(
 #'    erg = 0.12,
 #'    eri = 0.23,
 #'    scr = 0.34,
@@ -23,8 +95,8 @@
 #'  expected_mean_n_species <- 1.046121595
 #'  testthat::expect_equal(mean_n_species, expected_mean_n_species)
 #' @author Richel J.C. Bilderbeek
-#' @export
-pbd_numspec_mean_checked <- function(
+#' @noRd
+pbd_numspec_mean_checked_impl <- function(
   erg,
   eri,
   scr,
--- PBD/tests/testthat/test-pbd_numspec_checked.R
+++ PBD/tests/testthat/test-pbd_numspec_checked.R
@@ -1,83 +1,103 @@
 context("pbd_numspec_checked")
 
-test_that("pbd_numspec_mean_checked use", {
+test_that("pbd_numspec_mean_checked single-value use", {
 
-  expected <- pbd_numspec_mean_checked(
-      erg = 0.12,
-      eri = 0.23,
-      scr = 0.34,
-      sir = 0.45,
-      crown_age = 0.56
+  n <- pbd_numspec_mean_checked(
+    ergs = 0.1,
+    eris = 0.2,
+    scrs = 0.3,
+    sirs = 0.4,
+    crown_ages = 0.5
   )
-  expect_equal(expected, 1.046121595)
+  expect_equal(n, 1.029111871)
 
+  n <- pbd_numspec_mean_checked(
+    ergs = 0.2,
+    eris = 0.4,
+    scrs = 0.6,
+    sirs = 0.8,
+    crown_ages = 1.0
+  )
+  expect_equal(n, 1.440746567)
 })
 
-test_that("pbd_numspec_mean_checked abuse", {
+test_that("pbd_numspec_mean_checked vectorized use", {
+
+  ns <- pbd_numspec_mean_checked(
+    ergs = c(0.1, 0.2),
+    eris = c(0.2, 0.4),
+    scrs = c(0.3, 0.6),
+    sirs = c(0.4, 0.8),
+    crown_ages = c(0.5, 1.0)
+  )
+  expect_equal(ns, c(1.029111871, 1.440746567))
+})
+
+test_that("pbd_numspec_mean_checked single-value abuse", {
 
   expect_silent(
     pbd_numspec_mean_checked(
-      erg = 1.0,
-      eri = 1.0,
-      scr = 1.0,
-      sir = 1.0,
-      crown_age = 1.0
+      ergs = 1.0,
+      eris = 1.0,
+      scrs = 1.0,
+      sirs = 1.0,
+      crown_ages = 1.0
     )
   )
 
   expect_error(
     pbd_numspec_mean_checked(
-      erg = -123.456,
-      eri = 1.0,
-      scr = 1.0,
-      sir = 1.0,
-      crown_age = 1.0
+      ergs = -123.456,
+      eris = 1.0,
+      scrs = 1.0,
+      sirs = 1.0,
+      crown_ages = 1.0
     ),
-    "'erg' must be positive"
+    "All 'ergs' must be positive"
   )
 
   expect_error(
     pbd_numspec_mean_checked(
-      erg = 1.0,
-      eri = -123.456,
-      scr = 1.0,
-      sir = 1.0,
-      crown_age = 1.0
+      ergs = 1.0,
+      eris = -123.456,
+      scrs = 1.0,
+      sirs = 1.0,
+      crown_ages = 1.0
     ),
-    "'eri' must be positive"
+    "All 'eris' must be positive"
   )
 
   expect_error(
     pbd_numspec_mean_checked(
-      erg = 1.0,
-      eri = 1.0,
-      scr = -123.456,
-      sir = 1.0,
-      crown_age = 1.0
+      ergs = 1.0,
+      eris = 1.0,
+      scrs = -123.456,
+      sirs = 1.0,
+      crown_ages = 1.0
     ),
-    "'scr' must be positive"
+    "All 'scrs' must be positive"
   )
 
   expect_error(
     pbd_numspec_mean_checked(
-      erg = 1.0,
-      eri = 1.0,
-      scr = 1.0,
-      sir = -123.456,
-      crown_age = 1.0
+      ergs = 1.0,
+      eris = 1.0,
+      scrs = 1.0,
+      sirs = -123.456,
+      crown_ages = 1.0
     ),
-    "'sir' must be positive"
+    "All 'sirs' must be positive"
   )
 
   expect_error(
     pbd_numspec_mean_checked(
-      erg = 1.0,
-      eri = 1.0,
-      scr = 1.0,
-      sir = 1.0,
-      crown_age = -123.456
+      ergs = 1.0,
+      eris = 1.0,
+      scrs = 1.0,
+      sirs = 1.0,
+      crown_ages = -123.456
     ),
-    "'crown_age' must be positive"
+    "All 'crown_ages' must be positive"
   )
 })
 
